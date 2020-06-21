9de95494270e347b8ce69d23637d05cf782c923c
Frank Liang <liangfeng610@gmail.com>
2020-06-21 21:28:11 +1200

    refresh JWT Tokens in sever side

--- slack-clone-server/index.js
+++ slack-clone-server/index.js
@@ -3,12 +3,15 @@ import { ApolloServer } from 'apollo-server-express';
 import path from 'path';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import cors from 'cors';
+import models from './models';
+import jwt from 'jsonwebtoken';
+import { refreshTokens } from './auth';
+
 
 const PORT = 3003;
 const app = express();
-app.use(cors('*'));
 
-import models from './models';
+app.use(cors('*'));
 
 const SECRET = 'adfadfadgadgajdfa';
 const SECRET2 = 'adfadfadgadgajdfa1241312312#@$#%';
@@ -16,6 +19,28 @@ const SECRET2 = 'adfadfadgadgajdfa1241312312#@$#%';
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
+
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (error) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      if (newTokens.token && newTokens.refreshToken) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+}
+
+app.use(addUser);
 
 const server = new ApolloServer({
   typeDefs,
@@ -26,14 +51,12 @@ const server = new ApolloServer({
       'editor.theme': 'light'
     }
   },
-  context: {
+  context: ({ req }) => ({
     models,
-    user: {
-      id: 1
-    },
+    user: req.user,
     SECRET,
     SECRET2
-  }
+  })
 });
 
 
